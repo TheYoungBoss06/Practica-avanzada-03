a0de07a9c5d9eb418bf2dfd9a6021d2b9f630fe8
Wilson Espinal <theyoungboss00@gmail.com>
2024-11-29 00:47:40 -0400

    Se creo una funcion llamada calcular_resta que restara los datos  pedidos por input

--- Calculadora.py
+++ Calculadora.py
@@ -14,3 +14,20 @@ def calcular_suma(resultado):
         except ValueError:
             print('Por favor, Introduce numero valido: \n')
             return resultado
+        
+def calcular_resta(resultado):
+
+    while True:
+        try:
+            resta1 = int(input('Introduce el primer numero: '))
+            resta2 = int(input('Introduce el segundo numero:'))
+            resultado = resta1 - resta2
+            if resta1 and resta2:
+                print(f'El resultado de la Resta es: {resultado}:\n ')
+            else:
+                break
+            
+        except ValueError:
+            print('Por favor, Introduce numero valido: \n')
+            return resultado
+        
